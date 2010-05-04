8afe85283cbf9b00cba987569c540bcec187c212
Your <you@example.com>
2010-05-04 09:10:23 +0000

    added daily data and sichteroptin

--- create_flagged_data.py
+++ create_flagged_data.py
@@ -1,6 +1,37 @@
 
 
-def create_data(db, year, month):
+def create_data_daily(db, year, month, day):
+    """Creates the files with the per-month data in it. """
+
+    query = \
+    """
+    select count(*),fr_user from dewiki_p.flaggedrevs 
+    where 
+    fr_flags = 'dynamic'
+    and fr_timestamp like '%s%02d%02d%%'
+    group by fr_user
+    order by count(*);
+    """ % (year, month, day)
+
+    myfile = '/home/hroest/flagged_data/all_month_day%s%02d%02d'% (year, month, day )
+    f = open(myfile + '_tmp', 'w')
+    print "writing into " , f.name
+    cursor = db.cursor()
+    cursor.execute( query )
+    rows = cursor.fetchall()
+    f.write( 'count(*)\tfr_user\n' )
+    for r in rows:
+        f.write( '%s\t%s\n' % (r[0], r[1]) )
+    f.close()
+
+    #after closing we move the tmp file to the real location
+    import os
+    print 'moving now'
+    cmd  = 'mv %s %s' % (myfile + '_tmp', myfile )
+    print cmd
+    os.system( cmd )
+
+def create_data_monthly(db, year, month):
     """Creates the files with the per-month data in it. """
 
     query = \
--- nightly.py
+++ nightly.py
@@ -7,11 +7,13 @@ import sys
 sys.path.append( '/home/hroest/')
 sys.path.append( '/home/hroest/scripts/')
 import create_flagged_data
+import os
 
 today = datetime.date.today()
 now = datetime.datetime.now()
 this_year = today.year
 this_month = today.month
+this_day = today.day
 
 
 ###########################################################################
@@ -19,10 +21,19 @@ logfile = open('/home/hroest/nightly.log', 'a')
 logfile.write( '\nrun started:\n')
 logfile.write( '\tstart time %s\n' % now)
 
+os.system("sh /home/hroest/pywikipedia-folder/botpywikipedia/sichteroptin.sh")
+now = datetime.datetime.now()
+logfile.write( '\tfinished sichter: %s\n' % str(now) )
+
 #we create the new data for this month
 db = MySQLdb.connect(read_default_file="/home/hroest/.my.cnf")
-create_flagged_data.create_data( db, this_year, this_month )
+create_flagged_data.create_data_monthly( db, this_year, this_month )
 logfile.write( '\tupdated flagged files for %s%s\n' % (this_year, this_month) )
+
+#do the stats for yesterday
+create_flagged_data.create_data_daily( db, this_year, this_month, this_day -1 )
+logfile.write( '\tupdated flagged files for %s%s%s\n' % (this_year, 
+                                         this_month, this_day -1) )
 
 latest_file = '/home/hroest/flagged_data/latest_actualisation' 
 f = open(latest_file , 'w'); 
@@ -37,7 +48,7 @@ if today.day < 3:
     if old_month == 0:
         old_month = 12
         old_year = this_year - 1
-    create_flagged_data.create_data( db, old_year, old_month )
+    create_flagged_data.create_data_monthly( db, old_year, old_month )
     logfile.write( '\tupdated flagged files for %s%s\n' % (old_year, old_month))
 
 now = datetime.datetime.now()
